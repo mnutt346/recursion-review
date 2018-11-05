88c84886f82668b35370025d3d05a9520589814e
mnutt346 <mnutt346@gmail.com>
2018-11-05 16:15:09 -0600

    Pause

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj === "number") {
+    return obj.toString();
+  } else if(typeof obj === "boolean") {
+    if(obj === true) {
+    return "true";
+    } else {
+      return "false";
+    } 
+  } else if(obj === null) {
+    return "null";
+  } else if(typeof obj === "string"){
+    return '"' + obj + '"';
+  }else if(Array.isArray(obj)) {
+    if(obj.length <= 0) {
+      return '[]';
+    } else {
+      var toStr = [];
+      obj.forEach(function (elem) {
+        toStr = toStr.push(elem);
+      });
+      toStr = toStr.join('');
+      return '[' + toStr + ']';
+    }
+  }
 };

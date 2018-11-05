d1497de401e45fe73a78693cef25de5cfd76543d
mnutt346 <mnutt346@gmail.com>
2018-11-05 12:27:30 -0600

    we are just stopping

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var returnArray = [];
+  //var startNode = document.body
+  //console.log(startNode);
+  var nodeCheck = function(node) {
+    if(node.className === className) {
+      returnArray.push(node)
+    }
+    if(node.hasChildNodes()){
+      //console.log("has kids")
+      var children = node.childNodes
+      for(var i = 0; i < children.length; i++) {
+        //console.log('the ' + i + ' run')
+        nodeCheck(children[i])
+
+      }
+    }
+  }
+  nodeCheck(document.body)
+  //console.log('returnArray:')
+  console.log(returnArray)
+  return returnArray
 };
+
+// error 
+// function is outputting twice
+// fix this
+
